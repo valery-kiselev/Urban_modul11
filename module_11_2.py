d0d697d64244acca627a85087ceb0ddaf0e96aa4
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-10-12 21:26:53 +0400

    Add files via upload

---
+++ module_11_2.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+
+def introspection_info(obj):
+    obj_type = type(obj)
+    attr = dir(obj)
+    obj_met = [method for method in attr if callable(getattr(obj, method))]
+    obj_mod = obj.__class__.__module__
+    itog = {'type': obj_type, 'attributes': attr, 'methods': obj_met,
+         'module': obj_mod},
+    return itog
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        print(self.name, self.number_of_floors)
+
+h1 = House('ЖК Горский', 18)
+
+pprint(introspection_info(h1))
